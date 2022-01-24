e6fb421fab5f47053d9df4fe2725450ecb7d67fa
Criis21 <cristina_kirika@hotmail.com>
2022-01-24 19:52:58 +0000

    Contact and Copyright done!

--- src/front/js/component/Footer/Contact/Contact.jsx
+++ src/front/js/component/Footer/Contact/Contact.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Card, FloatingLabel, Button, Form } from "react-bootstrap";
 
 
 const Contact = () => {
     return (
         <>
-        
+            <container>
+                <Card className="text-center">
+                    <Card.Header>Subscribe/Newsletter</Card.Header>
+                    <Card.Body>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Email address"
+                        className="mb-3"
+                    >
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </FloatingLabel>
+                        <Button variant="primary">Contact</Button>
+                    </Card.Body>
+                </Card>
+            </container>
         </>
     );
 }
--- src/front/js/component/Footer/Copyright/Copyright.jsx
+++ src/front/js/component/Footer/Copyright/Copyright.jsx
@@ -1,1 +1,14 @@
-InfoContact
+import React from "react";
+import { } from "react-bootstrap";
+
+
+const Copyright = () => {
+    return (
+        <> 
+            <container className="container-fluid">
+                <h6 className="d-flex justify-content-center">Copyright</h6>
+            </container>
+        </>
+    );
+}
+export default Copyright;
