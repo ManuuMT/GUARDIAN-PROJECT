be1f135eaa5cf2997f8acb89926d41c6516675c5
Jesús Cano <jesus.cano@manumartinez.es>
2022-01-24 17:59:50 +0000

    General structure of Main component

---
+++ src/front/js/component/Main/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Features from "../Main/Features/Features.jsx";
+import HowWorks from "../Main/HowWorks/HowWorks.jsx";
+import Testimonials from "../Main/Testimonials/Testimonials.jsx";
+
+const Main = () => {
+
+    return (
+        <div>
+            <Features />
+			<HowWorks />
+			<Testimonials />
+        </div>
+    );
+};
+
+export default Main;
