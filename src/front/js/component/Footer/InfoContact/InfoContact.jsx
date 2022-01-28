5e21f77ad799daa937ead9407c1dc220e815d508
Criis21 <cristina_kirika@hotmail.com>
2022-01-28 17:01:39 +0000

    InfoContact branch

---
+++ src/front/js/component/Footer/InfoContact/InfoContact.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Row, Col, Table } from "react-bootstrap"
+
+
+const InfoContact = () => {
+    return (
+        <>
+            <container>
+            <Row>
+                <Col xs={12} md={4}>
+                </Col>
+                <Col xs={6} md={8}>
+                    <Table>
+                        <thead>
+                            <tr>
+                            <th>Link</th>
+                            <th>Legal</th>
+                            <th>Social</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            </tr>
+                            <tr>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            </tr>
+                            <tr>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            <td>Lorem</td>
+                            </tr>
+                        </tbody>
+                        </Table>
+                </Col>
+            </Row>
+            </container>
+        </>
+    );
+}
+export default InfoContact;
