51a8842de0e7ba2a1f2d67464122d9aa886513fb
Criis21 <cristina_kirika@hotmail.com>
2022-01-28 21:03:32 +0000

    Changed

--- src/front/js/component/Footer/Contact/Contact.jsx
+++ src/front/js/component/Footer/Contact/Contact.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-import { Card, FloatingLabel, Button, Form } from "react-bootstrap";
+import { FloatingLabel, Form } from "react-bootstrap";
 
 
 const Contact = () => {
     return (
         <>
-            <div className="container ">
-                <div className="row">
-                <Card className="card-btn mt-5 mb-5 text-center">
-                    <Card.Header><h2 className="mt-4">Subscribe/Newsletter</h2></Card.Header>
-                    <Card.Body>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Email address"
-                        className="w-50 btn grad-btn card-btn">
-                        <Form.Control type="email" placeholder="name@example.com"/>
+                <div className="containe-fluid w-50 ms-auto me-auto mb-5">
+                    <div className="row">
+                        <div className="text-center">
+                        <h3 className="left-col">Subscribe Our <strong className="grad">Newsletter</strong></h3>
+                        <h3 className="left-col"> For Latest Updates</h3>
+                        </div>
+                        <div>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Email address"
+                            className="mb-3">
+                            <Form.Control type="email" placeholder="name@example.com" />
                         </FloatingLabel>
-                            <div>
-                                <Button className="btn border-gradient intro-btn mt-3" >Enviar</Button>
-                            </div>
-                    </Card.Body>
-                </Card>
+                        </div>
+                        <div>
+                        <button className="btn grad-btn">Subscribe</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
         </>
     );
 }
