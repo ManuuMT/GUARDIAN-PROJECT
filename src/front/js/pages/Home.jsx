3bf4b2c27e104229621c2e37099bbc2d7b586837
Criis21 <cristina_kirika@hotmail.com>
2022-01-28 17:03:21 +0000

    New component in Home.jsx

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Header from "../component/Header/Header.jsx"
+import Footer from "../component/Footer/Footer.jsx";
 
 
 const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-	    <Header/>
+	    <Header/>,
+		<Footer/>
 	);
 };
 
