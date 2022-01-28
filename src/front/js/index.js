7f8f9d79ed5547f32d4c8d5334244a57746aa781
Criis21 <cristina_kirika@hotmail.com>
2022-01-28 17:04:19 +0000

    new import in index.js

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/index.scss";
 import Layout from "./layout";
 
