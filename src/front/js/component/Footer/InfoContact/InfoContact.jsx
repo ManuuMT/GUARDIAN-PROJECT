4bdac024b5ca3feecd3f3c428b13e2a8068ef705
Criis21 <cristina_kirika@hotmail.com>
2022-01-31 19:20:43 +0000

    Part of InfoContact

--- src/front/js/component/Footer/InfoContact/InfoContact.jsx
+++ src/front/js/component/Footer/InfoContact/InfoContact.jsx
@@ -6,43 +6,25 @@ import logo from '../../../../assets/img/logo-ok.png';
 const InfoContact = () => {
     return (
         <>
-            <div className="container">
+            <div className="container-fluid">
                 <Row>
                     <Col xs={12} md={4}>
                         <div className="container m-auto">
-                            <p className=""><img src={logo} className="logo"/></p>
-                            <h5 className="left-col text-center mt-5">"Feel safe walking</h5>
-                            <h5 className="left-col text-center">around your city"</h5>
+                            <div>
+                                <div>
+                                <a className="navbar-brand left-col"><img src={logo} className="logo"/> <strong>Guardian</strong> </a>
+                                </div>
+                                <div>
+                                    <h6></h6>
+                                </div>
+                        </div>
+                            <h5 className="left-col navbar-brand">"Feel safe walking</h5>
+                            <h5 className="left-col navbar-brand">around your city"</h5>
                         </div>
                         <p className="text-light"></p>
                     </Col>
                     <Col xs={6} md={8}>
-                        <Table>
-                            <thead>
-                                <tr>
-                                <th>Link</th>
-                                <th>Legal</th>
-                                <th>Social</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                </tr>
-                                <tr>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                </tr>
-                                <tr>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                <td>Lorem</td>
-                                </tr>
-                            </tbody>
-                            </Table>
+
                     </Col>
             </Row>
             </div>
