a93c271c9d5f2aa0fcb7978a92ae941953e3b226
Criis21 <cristina_kirika@hotmail.com>
2022-01-31 20:26:34 +0000

    InfoContact

--- src/front/js/component/Footer/InfoContact/InfoContact.jsx
+++ src/front/js/component/Footer/InfoContact/InfoContact.jsx
@@ -6,28 +6,65 @@ import logo from '../../../../assets/img/logo-ok.png';
 const InfoContact = () => {
     return (
         <>
-            <div className="container-fluid">
-                <Row>
-                    <Col xs={12} md={4}>
-                        <div className="container m-auto">
-                            <div>
+                <div className="container-fluid mt-5">
+                    <Row>
+                        <Col xs={12} md={4}>
+                            <div className="container">
                                 <div>
-                                <a className="navbar-brand left-col"><img src={logo} className="logo"/> <strong>Guardian</strong> </a>
-                                </div>
-                                <div>
-                                    <h6></h6>
-                                </div>
-                        </div>
-                            <h5 className="left-col navbar-brand">"Feel safe walking</h5>
-                            <h5 className="left-col navbar-brand">around your city"</h5>
-                        </div>
-                        <p className="text-light"></p>
-                    </Col>
-                    <Col xs={6} md={8}>
-
-                    </Col>
-            </Row>
-            </div>
+                                    <div>
+                                    <a className="navbar-brand left-col"><img src={logo} className="logo"/> <strong>Guardian</strong> </a>
+                                    </div>
+                                    <div>
+                                        <h6></h6>
+                                    </div>
+                            </div>
+                            <p className="mt-3 left-col">Feel safe walking around your city</p>
+                            </div>
+                            <p className="text-light"></p>
+                        </Col>
+                        <Col xs={6} md={8}>
+                            <Table className="table table-borderless">
+                                <thead>
+                                    <tr className="left-col">
+                                    <th>Link</th>
+                                    <th>Legal</th>
+                                    <th>Social</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="left-col">
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li><a className="text-light text-decoration-none" href="">Innovation</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Map</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">How works</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Features</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Testimonials</a></li>
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li><a className="text-light text-decoration-none" href="">Terms of use</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Terms of conditions</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Privacy Policy</a></li>
+                                                <li><a className="text-light text-decoration-none" href="">Cookie Policy</a></li>
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul className="list-unstyled">
+                                                <li>Hello</li>
+                                                <li>Hello</li>
+                                                <li>Hello</li>
+                                                <li>Hello</li>
+                                                <li>Hello</li>
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+                </Row>
+                </div>
         </>
     );
 }
