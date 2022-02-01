58169c25b7aa0957292e1871ffa0678faa094e6d
Criis21 <cristina_kirika@hotmail.com>
2022-02-01 11:48:59 +0000

    InfoContact done!

--- src/front/js/component/Footer/InfoContact/InfoContact.jsx
+++ src/front/js/component/Footer/InfoContact/InfoContact.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Row, Col, Table } from "react-bootstrap"
+import "./InfoContact.scss";
 import logo from '../../../../assets/img/logo-ok.png';
+import { AiFillFacebook, AiFillLinkedin, AiFillYoutube, AiFillInstagram, AiTwotoneMail } from "react-icons/ai";
+import { BsTelephoneFill } from "react-icons/bs"
+
+
 
 
 const InfoContact = () => {
@@ -22,10 +27,11 @@ const InfoContact = () => {
                             </div>
                             <p className="text-light"></p>
                         </Col>
-                        <Col xs={6} md={8}>
+                        <Col xs={1} md={8}>
                             <Table className="table table-borderless">
                                 <thead>
                                     <tr className="left-col">
+                                    <th>Contacto</th>
                                     <th>Link</th>
                                     <th>Legal</th>
                                     <th>Social</th>
@@ -34,7 +40,13 @@ const InfoContact = () => {
                                 <tbody>
                                     <tr className="left-col">
                                         <td>
-                                            <ul className="list-unstyled">
+                                            <ul className="list-unstyled me-2">
+                                                <li className="pb-3"><a className="text-light text-decoration-none" href=""><AiTwotoneMail className="icon"></AiTwotoneMail>info@theguardian.es</a></li>
+                                                <li><a className="text-light text-decoration-none" href=""><BsTelephoneFill className="icon"></BsTelephoneFill>+34 640 222 036</a></li>
+                                            </ul>
+                                        </td>
+                                        <td>
+                                            <ul className="list-unstyled me-5">
                                                 <li><a className="text-light text-decoration-none" href="">Innovation</a></li>
                                                 <li><a className="text-light text-decoration-none" href="">Map</a></li>
                                                 <li><a className="text-light text-decoration-none" href="">How works</a></li>
@@ -43,20 +55,19 @@ const InfoContact = () => {
                                             </ul>
                                         </td>
                                         <td>
-                                            <ul className="list-unstyled">
-                                                <li><a className="text-light text-decoration-none" href="">Terms of use</a></li>
-                                                <li><a className="text-light text-decoration-none" href="">Terms of conditions</a></li>
-                                                <li><a className="text-light text-decoration-none" href="">Privacy Policy</a></li>
-                                                <li><a className="text-light text-decoration-none" href="">Cookie Policy</a></li>
+                                            <ul className="list-unstyled me-2">
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href="">Terms of use</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href="">Terms of conditions</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href="">Privacy Policy</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href="">Cookie Policy</a></li>
                                             </ul>
                                         </td>
                                         <td>
                                             <ul className="list-unstyled">
-                                                <li>Hello</li>
-                                                <li>Hello</li>
-                                                <li>Hello</li>
-                                                <li>Hello</li>
-                                                <li>Hello</li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href=""><AiFillFacebook className="icon"></AiFillFacebook>Facebook</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href=""><AiFillLinkedin className="icon"></AiFillLinkedin>Linkedin</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href=""><AiFillYoutube className="icon"></AiFillYoutube>Youtube</a></li>
+                                                <li className="pb-2"><a className="text-light text-decoration-none" href=""><AiFillInstagram className="icon"></AiFillInstagram>Instagram</a></li>
                                             </ul>
                                         </td>
                                     </tr>
