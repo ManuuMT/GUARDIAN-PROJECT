a2e6ea0b87ef78305956a334ed889685bd1d00fe
Jesús Cano <jesus.cano@manumartinez.es>
2022-02-09 17:41:14 +0000

    Fixing conflicts

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import Header from "../component/Header/Header.jsx"
-import Main from "../component/Main/Main.jsx"
+//import Header from "../component/Header/Header.jsx"
+//import Main from "../component/Main/Main.jsx"
 
 
 const Home = () => {
