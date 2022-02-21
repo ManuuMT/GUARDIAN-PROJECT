eace3d9ef5004a0401d4f8d8722e66c042db06dd
ManuuMT <ema14mt@gmail.com>
2022-02-21 19:27:19 +0000

    Test model

--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Incident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    reported_by = db.Column(db.String(80), unique=False, nullable=False)
+    category = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+         return f"{self.category}"
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "reported_by": self.reported_by,
+            "category": self.category,
+            "description": self.description
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,15 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Incident
 from api.utils import generate_sitemap, APIException
+
+#import os
+#from flask import Flask, request, jsonify, url_for
+#from flask_migrate import Migrate
+#from flask_swagger import swagger
+#from flask_cors import CORS
+#from utils import APIException, generate_sitemap
+#from admin import setup_admin
+#from models import db, Incident
 
 api = Blueprint('api', __name__)
 
@@ -12,7 +18,7 @@ api = Blueprint('api', __name__)
 def handle_hello():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Hello! I'm a message that came from the GUARDIAN backend"
     }
 
     return jsonify(response_body), 200
