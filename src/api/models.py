14f96ffa724e72c95db2f03da5ea0a968fbadc77
ManuuMT <ema14mt@gmail.com>
2022-02-21 20:08:53 +0000

    Long and Lat columns added to model

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,8 @@ class Incident(db.Model):
     reported_by = db.Column(db.String(80), unique=False, nullable=False)
     category = db.Column(db.String(120), unique=False, nullable=False)
     description = db.Column(db.String(120), unique=False, nullable=True)
+    longitude = db.Column(db.String(120), unique=False, nullable=False)
+    latitude = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self):
          return f"{self.category}"
@@ -16,5 +18,7 @@ class Incident(db.Model):
             "id": self.id,
             "reported_by": self.reported_by,
             "category": self.category,
-            "description": self.description
+            "description": self.description,
+            "longitude": self.longitude,
+            "latitude": self.latitude
         }
