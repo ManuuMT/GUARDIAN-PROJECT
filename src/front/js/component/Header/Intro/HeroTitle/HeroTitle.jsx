48e103310b1b326612189ed5f605bf881ff3f212
Jesús Cano <jesus.cano@manumartinez.es>
2022-02-21 19:11:38 +0000

    Editing home page view

--- src/front/js/component/Header/Intro/HeroTitle/HeroTitle.jsx
+++ src/front/js/component/Header/Intro/HeroTitle/HeroTitle.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const HeroTitle = () => {
     return (<>
-            <h1>Bienvenido a <span className="grad">GUARDIAN</span></h1> 
-            <h1>innovation and security</h1>
-            <p className="mt-3">In the world of Cryptocurrency, a node is a computer that connects to a cryptocurrency network.</p>
-            <button className="btn border-gradient intro-btn mt-3">Get Started</button>
+            
+            <div className="container pt-5">
+                <h1>Bienvenido a <span className="grad">GUARDIAN</span></h1> 
+                <h1>innovación y seguridad</h1>
+                <p className="mt-3">Buscamos hacer las calles más seguras para los usuarios ofreciendo una 
+                representación gráfica de los diferentes índices de criminalidad donde mostrar
+                el nivel de seguridad en nuestras ciudades.</p>
+                <button className="btn grad-btn intro-btn mt-3">Get Started</button>
+            </div>
         </>
     );
 };
--- src/front/js/component/Main/Features/Features.jsx
+++ src/front/js/component/Main/Features/Features.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import FeatureItem from "./FeatureItem/FeatureItem.jsx";
 import "./Features.scss"
 
+import { AiOutlineDatabase } from "react-icons/ai"
+import { AiOutlineUsergroupAdd } from "react-icons/ai"
+import { FaRegMap } from "react-icons/fa"
+
+
 const Features = () => {
     return (
       <>
         <div className="container-fluid features-background-color">
           <div className="container px-4 py-5 text-white ">
-            <h2 className="pb-2 text-center">Safety advanced <span className="grad">features</span></h2>
-            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
-              <FeatureItem title="Feature 1" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et magna aliquet, ultrices nulla condimentum, congue orci. Sed efficitur rutrum felis, ut molestie diam dapibus vitae."/>
-              <FeatureItem title="Feature 2" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et magna aliquet, ultrices nulla condimentum, congue orci. Sed efficitur rutrum felis, ut molestie diam dapibus vitae."/>
-              <FeatureItem title="Feature 3" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et magna aliquet, ultrices nulla condimentum, congue orci. Sed efficitur rutrum felis, ut molestie diam dapibus vitae."/>                    
+            <h2 className="pb-2 text-center">Nuestras <span className="grad">funcionalidades</span> de seguridad</h2>
+            
+            <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">              
+              <FeatureItem icon={<AiOutlineDatabase className="icon-feature" size={45}/>} title="Actualización" text="Guardian es una plataforma que muestra los datos en tiempo real y de forma contínua, brindando así una experiencia inmediata a los usuarios, independientemente del lugar y el momento en el que se encuentren."/>
+              <FeatureItem icon={<AiOutlineUsergroupAdd className="icon-feature" size={45}/>} title="Aportación" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et magna aliquet, ultrices nulla condimentum, congue orci. Sed efficitur rutrum felis, ut molestie diam dapibus vitae."/>
+              <FeatureItem icon={<FaRegMap className="icon-feature" size={45}/>} title="Interpretación" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et magna aliquet, ultrices nulla condimentum, congue orci. Sed efficitur rutrum felis, ut molestie diam dapibus vitae."/>                    
             </div>
           </div>
         </div>
