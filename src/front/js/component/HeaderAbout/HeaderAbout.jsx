356091e7a7fc45986763678e4fb4bc66101498fb
Criis21 <cristina_kirika@hotmail.com>
2022-02-23 20:35:07 +0000

    InfoAbout, IconsAbout

--- src/front/js/component/HeaderAbout/HeaderAbout.jsx
+++ src/front/js/component/HeaderAbout/HeaderAbout.jsx
@@ -1,19 +1,43 @@
 import React from "react";
-import {  } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
 import "./HeaderAbout.scss";
+import about1 from '../../../assets/img/HeaderAbout/about1.jpg';
+
 const HeaderAbout = () => {
     return (
 
         <>
             <div className="container">
-                <div class="text-center">
-                    <div class="card-body bg-img">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn grad-btn mt-2">Go somewhere</a>
-                    </div>
-                </div>
+                                <Card className="cardHeaderAbout">
+                                        <Card.Img src={about1} alt="Card image" className="imgHeader"/>
+                                                <Card.ImgOverlay className="titleHeader">
+                                                    
+                                                        
+                                                            <Card.Title className="justify-content-center mx-auto">
+                                                                <h3>¿Quieres saber más sobre nosotros?<br /><br /></h3>
+                                                            </Card.Title>
+                                                            
+                                                       
+                                                    
+                                                            <Card.Text className="md-2">
+                                                            Guardian es la primera aplicación móvil de ayuda al ciudadano gratuita. 
+                                                            A través de la aplicación, nuestros usuarios pueden 
+                                                            saber en todo momento donde se encuentran, si existe algún peligro
+                                                            cercano, podrán ponerse en contacto con la polícia o los servicios de 
+                                                            emergencia sin necesidad de dar su ubicación ya que, Guardian, lleva tu 
+                                                            ubicación activa en todo momento. <br /><br />
+
+                                                            En definitiva, saber si están bien. La idea de Guardian es de Emnuel Maldonado, 
+                                                            Jesús Cano y Cristina Fernández.<br /><br />
+                                                            </Card.Text>
+                                                       
+                                                    
+                                                            <a href="#" className="btn grad-btn mx-auto">Go somewhere</a>
+                                                        
+                                                </Card.ImgOverlay>
+                                </Card>
             </div>
+
 
 
         </>
--- src/front/js/component/InfoAbout/InfoAbout.jsx
+++ src/front/js/component/InfoAbout/InfoAbout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col, Card } from "react-bootstrap";
 
 
 const InfoAbout = () => {
@@ -6,6 +7,32 @@ const InfoAbout = () => {
 
         <>
         
+        <div className="container rowBorder">
+        <Row> 
+            <Col md={4}><img src="https://img.remediosdigitales.com/793a7d/people-2557396_640-2-/840_560.jpg" alt="" className="rowBorder"/></Col>
+            <Col md={{ span: 5, offset: 3 }} className="rowBorder">
+                <div>
+                    <Card.Title className="justify-content-center mx-auto">
+                        <h3>¿Quieres saber más sobre nosotros?<br /><br /></h3>
+                    </Card.Title>
+
+                </div>
+                <div>
+                    <Card.Text className="md-2">
+                        Guardian es la primera aplicación móvil de ayuda al ciudadano gratuita. 
+                        A través de la aplicación, nuestros usuarios pueden 
+                        saber en todo momento donde se encuentran, si existe algún peligro
+                        cercano, podrán ponerse en contacto con la polícia o los servicios de 
+                        emergencia sin necesidad de dar su ubicación ya que, Guardian, lleva tu 
+                        ubicación activa en todo momento. <br /><br />
+
+                        En definitiva, saber si están bien. La idea de Guardian es de Emnuel Maldonado, 
+                        Jesús Cano y Cristina Fernández.<br /><br />
+                    </Card.Text>
+                </div>
+            </Col>
+        </Row>
+        </div>
         
         </>
 
