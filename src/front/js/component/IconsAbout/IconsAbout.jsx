e98b662899cb2446947578acdb65754448c77bb5
Jesús Cano <jesus.cano@manumartinez.es>
2022-02-25 12:07:52 +0000

    Fixing details about component

--- src/front/js/component/IconsAbout/IconsAbout.jsx
+++ src/front/js/component/IconsAbout/IconsAbout.jsx
@@ -10,41 +10,41 @@ const IconsAbout = () => {
         
         <div>
             <div className="container text-center mt-5">
-                <div className="card-group">
-                    <div className="card m-3 rounded">
+                <div className="card-group text-white">
+                    <div className="card m-3 rounded bg-icons">
                         <div className="card-body">
                             <div className="m-auto firstIcons btn grad-btn mt-2">
                                 <AiFillCheckCircle className="icon" icon={"spinner"}></AiFillCheckCircle>
                             </div>
                             <p className="card-text pt-4">99%</p>
-                            <p className="card-text"><small className="text-muted">Clientes nuevos</small></p>
+                            <p className="card-text"><small className="">Clientes nuevos</small></p>
                         </div>
                     </div>
-                    <div className="card m-3 rounded">
+                    <div className="card m-3 rounded bg-icons">
                         <div className="card-body ">
                             <div className="m-auto firstIcons btn grad-btn mt-2">
                                 <BsFillCalendarWeekFill className="icon" icon={"spinner"}></BsFillCalendarWeekFill>
                             </div>
                             <p className="card-text pt-4">3 +</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <p className="card-text"><small className="">Last updated 3 mins ago</small></p>
                         </div>
                     </div>
-                    <div className="card m-3 rounded">
+                    <div className="card m-3 rounded bg-icons">
                         <div className="card-body">
                             <div className="m-auto firstIcons btn grad-btn mt-2">
                                 <AiFillFlag className="icon" icon={"spinner"}></AiFillFlag>
                             </div>
                             <p className="card-text pt-4">17</p>
-                            <p className="card-text"><small className="text-muted">Nuevos pins</small></p>
+                            <p className="card-text"><small className="">Nuevos pins</small></p>
                         </div>
                     </div>
-                    <div className="card m-3 rounded">
+                    <div className="card m-3 rounded bg-icons">
                         <div className="card-body">
                             <div className="m-auto firstIcons btn grad-btn mt-2">
                                 <BsPeopleFill className="icon" icon={"spinner"}></BsPeopleFill>
                             </div>
                             <p className="card-text pt-4">13k</p>
-                            <p className="card-text"><small className="text-muted">Guardianes</small></p>
+                            <p className="card-text"><small className="">Guardianes</small></p>
                         </div>
                     </div>
                 </div>
--- src/front/js/component/InfoAbout/InfoAbout.jsx
+++ src/front/js/component/InfoAbout/InfoAbout.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import "./InfoAbout.scss";
-import { Card } from "react-bootstrap";
-
-
 
 const InfoAbout = () => {
     return (
 
         <>
-        <div className="container  mt-5">
-            <div className="clearfix">
-                <img src="https://negociosyempresa.com/wp-content/uploads/2021/05/international-team-of-coworkers-putting-colorful-puzzles-together-picture-id1201193769.jpg" className="img-fluid col-md-6 float-md-end mb-3 ms-md-3 rounded" alt="..."/>
+            <div className="container py-5">            
+                <div className="row">
+                        <div className="col-xl-6 col-lg-12">
+                            <h3 className="text-white">¿Quieres saber más sobre nosotros?<br /><br /></h3>
+                            <p className="text-light">
+                                Guardian es la primera aplicación móvil de ayuda al ciudadano gratuita.<br /><br />
+                                A través de la aplicación, nuestros usuarios pueden 
+                                saber en todo momento donde se encuentran, si existe algún peligro
+                                cercano, podrán ponerse en contacto con la polícia o los servicios de 
+                                emergencia sin necesidad de dar su ubicación ya que, Guardian, lleva tu 
+                                ubicación activa en todo momento. <br /><br />
 
-                <Card.Title className="justify-content-center mx-auto text-light">
-                        <h3>¿Quieres saber más sobre nosotros?<br /><br /></h3>
-                    </Card.Title>
-
-                    <Card.Text className="text-light">
-                        Guardian es la primera aplicación móvil de ayuda al ciudadano gratuita.<br /><br />
-                        A través de la aplicación, nuestros usuarios pueden 
-                        saber en todo momento donde se encuentran, si existe algún peligro
-                        cercano, podrán ponerse en contacto con la polícia o los servicios de 
-                        emergencia sin necesidad de dar su ubicación ya que, Guardian, lleva tu 
-                        ubicación activa en todo momento. <br /><br />
-
-                        En definitiva, saber si están bien. La idea de Guardian es de Emnuel Maldonado, 
-                        Jesús Cano y Cristina Fernández.<br /><br />
-                    </Card.Text>
+                                En definitiva, saber si están bien. La idea de Guardian es de Emanuel Maldonado, 
+                                Jesús Cano y Cristina Fernández.<br /><br />
+                            </p>
+                        </div>
+                        <div className="col-xl-6 col-lg-12">
+                            <img className="img-fluid rounded" src="https://negociosyempresa.com/wp-content/uploads/2021/05/international-team-of-coworkers-putting-colorful-puzzles-together-picture-id1201193769.jpg" alt="..."/>
+                        </div>
+                </div>
             </div>
-        </div>
         
         </>
 
