d3773807e64868e39f0d876b15b449d1dac5c4f5
ManuuMT <ema14mt@gmail.com>
2022-03-16 02:21:20 +0000

    QuickMap restored to Home

--- src/front/js/component/Header/Intro/Intro.jsx
+++ src/front/js/component/Header/Intro/Intro.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Intro.scss"
 import HeroTitle from "./HeroTitle/HeroTitle.jsx"
-//import TestMap from "./Maps/TestMap";
+import TestMap from "./Maps/TestMap.jsx";
 //import MapForm from "./MapForm/MapForm.jsx"
 
 const Intro = () => {
+    let quickmap = {height: "50vh", width: "80%"};
   return (
 <div className="container mt-5">
     <div className="row intro-row">
@@ -14,7 +15,8 @@ const Intro = () => {
         </div>
        
         <div className="quickmap col-md-6 col-sm-12 right-col">
-            {/*<TestMap mapContainerStyle={"height: 70vh, width: 100%"}/>*/}
+            <TestMap style={quickmap}/>
+            
         </div>
     </div>
 </div>
